f5d21a7fd3a203b44fb4cbdf9c1b02ee5fe22901
bip1n <beepyn98166@gmail.com>
2023-12-20 10:07:15 +0545

    Updated AboutMe info

--- src/components/Aboutme.jsx
+++ src/components/Aboutme.jsx
@@ -1,4 +1,4 @@
-import { user, facebook, github } from "../assets";
+import { user, facebook, github, instagram, linkedin } from "../assets";
 import styles, { layout } from "../style";
 
 const Aboutme = () => (
@@ -17,14 +17,17 @@ const Aboutme = () => (
         About Me <br className="sm:block hidden" /> 
       </h2>
       <p className={`${styles.paragraph} max-w-[600px] mt-5`}>
-      Yeha chai mero barema badhai chadhai garera 1 paragraph lekhnu parchha.
+        I'm Bhabishwar Khanal (Bipin). I'm a Software Engineering student at NCIT, Lalitpur. I love to call myself a technophile. I love exploring every field of technology.
       </p>
       <p className={`${styles.paragraph} max-w-[600px] mt-5`}>
-        Find me on :
+       My Socail Handles:
       </p>
       <div className="flex flex-row flex-wrap sm:mt-10 mt-6">
-        <img src={github} alt="google_play" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" />
-        <img src={facebook} alt="google_play" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" />
+        <a href="https://www.instagram.com/bipinwhooo/" target="_blank"><img src={instagram} alt="instagram" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" /></a>
+        <a href="https://www.facebook.com/bipinkhanal69/" target="_blank"> <img src={facebook} alt="Facebook" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" /></a>
+        <a href="https://github.com/bip1n" target="_blank"> <img src={github} alt="Github" className="w-[128.86px] h-[42.05px] object-contain mr-5 cursor-pointer" /></a>
+        <a href="https://www.linkedin.com/in/bipin-khanal-03b257228/" target="_blank"><img src={linkedin} alt="LinkedIN" className="w-[144.17px] h-[43.08px] object-contain cursor-pointer" /></a>
+
       </div>
     </div>
   </section>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,15 +9,15 @@ const Hero = () => {
 
         <div className="flex flex-row justify-between items-center w-full">
           <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
-            Hey, It's <br className="sm:flex hidden" />{" "}
-            <span className="text-gradient">Mero Name</span>{" "}
+            Hey, It's <br></br>
+            <div  className="text-gradient">Bhabishwar Khanal</div>{" "}
           </h1>
           <div className="ss:flex hidden md:mr-4 mr-0">
           </div>
         </div>
 
-        <h1 className="font-poppins font-semibold ss:text-[38px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
-         - Yeha Kehi Lekhnu Parchha!!!
+        <h1 className="font-poppins font-semibold ss:text-[28px] text-[32px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+         - Desperate To Learn!!!
         </h1>
        
       </div>
